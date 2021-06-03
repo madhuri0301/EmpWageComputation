89745986ab3491d3e232a915e3096365830eb135
Madhuri <harshupatil0301@gmail>
2021-06-03 06:55:29 -0700

    Calculate Wage Of Maximum Work Hours For Month

--- EmpWageComputation/Employee.cs
+++ EmpWageComputation/Employee.cs
@@ -9,15 +9,18 @@ namespace EmpWageComputation
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
         const int EMP_RATE_PER_HOUR = 20;
-        const int N0_OF_WORKING_DAYS = 20;
+        const int NO_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
 
-        int empHour = 0;
-        int empWage = 0;
         int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
         public void Attendence()
         {
-            for (int day = 0; day < N0_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_WORKING_DAYS)
             {
+                int empHour = 0;
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -37,7 +40,8 @@ namespace EmpWageComputation
                         empHour = 0;
                         break;
                 }
-                empWage = empHour * EMP_RATE_PER_HOUR;
+                totalEmpHrs += empHour;
+                int empWage = empHour * EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
                 Console.WriteLine("Employee Wage is :" + empWage);
             }
