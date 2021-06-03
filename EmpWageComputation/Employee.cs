cfac442d9709e8984dad5bbce1b0fe3ece24d6dc
Madhuri <harshupatil0301@gmail>
2021-06-03 09:38:11 -0700

    Solving Problem Using Opps Concept

--- EmpWageComputation/Employee.cs
+++ EmpWageComputation/Employee.cs
@@ -6,17 +6,17 @@ namespace EmpWageComputation
 {
     class Employee
     {
-        const int IS_FULL_TIME = 1;
-        const int IS_PART_TIME = 2;
-        const int EMP_RATE_PER_HOUR = 20;
-        const int NO_WORKING_DAYS = 20;
-        const int MAX_HRS_IN_MONTH = 100;
-
-        int totalEmpWage = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
         public void Attendence()
         {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NO_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_MONTH = 100;
+
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_WORKING_DAYS)
             {
                 int empHour = 0;
